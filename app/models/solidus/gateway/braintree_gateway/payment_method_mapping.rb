b6c64bbae8163ac14782d9ab1524d699583d5ae2
Igor Pstyga <igor.pstyga@gmail.com>
2017-02-24 00:10:25 -0800

    Add Apple Pay Card mapping

--- app/models/solidus/gateway/braintree_gateway/payment_method_mapping.rb
+++ app/models/solidus/gateway/braintree_gateway/payment_method_mapping.rb
@@ -46,6 +46,24 @@ module Solidus
         end
       end
 
+      class ApplePayCard < Base
+        APPLE_CARD_TYPE_MAPPING = {
+          Braintree::ApplePayCard::CardType::Visa => "apple_pay_visa",
+          Braintree::ApplePayCard::CardType::AmEx => "apple_pay_american_express",
+          Braintree::ApplePayCard::CardType::MasterCard => "apple_pay_master_card",
+          "Apple Pay - Discover" => "apple_pay_discover",
+        }
+
+        def to_hash
+          {
+            cc_type: APPLE_CARD_TYPE_MAPPING[source.card_type],
+            month: source.expiration_month,
+            year: source.expiration_year,
+            last_digits: source.source_description
+          }
+        end
+      end
+
       class NullRepresenter < Base
         def to_hash
           {}
